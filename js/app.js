9ebdbbc79bf6689a0c50e2410c72609835f72a64
V. Kirichenko <fkupme@yandex.ru>
2024-01-21 20:26:01 +0300

    mobile adaptiv and slider added

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const trendsSwiper = new Swiper('.trends-slider', {
+	direction: 'horizontal',
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
+const blogSwiper = new Swiper('.blog-slider', {
+	direction: 'horizontal',
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
+const gallerySwiper = new Swiper('.gallery-slider', {
+	direction: 'horizontal',
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
+
+
+
